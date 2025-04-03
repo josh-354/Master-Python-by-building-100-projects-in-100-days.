435d580266f64564fe8a74b76dc1e3c43a04724c
josh-354 <bryllejoshua354@gmail.com>
2025-04-03 14:47:27 +0800

    Tip calculator(day 2).py

--- Tip calculator(day 2).py
+++ Tip calculator(day 2).py
@@ -1,1 +1,9 @@
+print("Welcome to the tip calculator")
+total_bill = float(input("what was the total bill"))
+tip = int(input("what percentage tip would you like to give?"))
+people_split = int(input("how many people to split the bill?"))
+
+bill_with_tip=tip/100*total_bill+total_bill
+
+print(bill_with_tip)
 
